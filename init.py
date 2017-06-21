be56c9c1385e6850f42807c491d717c693772bed
Santosh Kumar <me@sntsh.com>
2017-06-21 12:49:08 +0530

    Sets bbox to B by default
    Sets bounding box from A+B to B. Reducing the render time.
    See init.py:5 for explanation.

--- init.py
+++ init.py
@@ -1,3 +1,6 @@
 # Project Settings > Default format: HD 1920x1080
 nuke.knobDefault("Root.format", "HD_1080")
 
+# Sets the bounding box to B instead of A and B combined
+# See https://twitter.com/sntshk/status/877413861254725635
+nuke.knobDefault("Merge.bbox", "3")
