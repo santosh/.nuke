2e4588503e5fed063ceba34c48f0ddf4460bea01
Santosh Kumar <me@sntsh.com>
2017-06-29 05:41:39 +0530

    prints PySide and QtCore version at startup

--- init.py
+++ init.py
@@ -1,4 +1,9 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
+import PySide
+from PySide import QtCore
+
+print("Nuke is starting with PySide", PySide.__version__,
+	"and QtCore version ", QtCore.qVersion())
 
 # Project Settings > Default format: HD 1920x1080
 nuke.knobDefault("Root.format", "HD_1080")
