80ab7f1409b53d0802b417c11ebdb2604a5fb886
Santosh Kumar <me@sntsh.com>
2017-08-18 16:28:15 +0530

    same thing, but with expression

--- dupvalue.py
+++ dupvalue.py
@@ -4,3 +4,7 @@ ccNodes = [i for i in nuke.allNodes() if i.name().startswith('ColorCorrect')]
 
 for i in ccNodes:
     i.knob('saturation').setValue(masterSetting)
+    
+    # below line does the same thing, but instead of doing silently,
+    # it visually draws a green line between them
+    #i.knob('saturation').setExpression(MasterNode.saturation)
