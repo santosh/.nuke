85a986031140d648aa251257d7cb60706a10f181
Santosh Kumar <me@sntsh.com>
2017-08-24 05:21:41 +0530

    adds dummy commands
    inside:
     * Nodes menu (Toolbar)
     * $antosh Menu (Menubar)
     * Edit/Node (Or any other nuke menu)

--- menu.py
+++ menu.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
+import dummy
 
 # Eliminates the frustration of 10 panels, defaults to 2
 
 maxPanels = nuke.toNode('preferences')['maxPanels']
 maxPanels.setValue(2)
 
-# Santosh Menubar
-SantoshMenu = menubar.addMenu("Santosh")
+# Nuke's Own Menus
+# This will add dummy under Edit/Node menu
+# this is the top nuke menubar
+nk = nuke.menu('Nuke')
+# adds custom item in existing menu
+nk.findItem('Edit/Node').addCommand('Dummy', 'nuke.createNode("Write")', 'ctrl+y', index=0)
 
-# Santosh Toolbar
-toolbar = nuke.toolbar('Nodes')
-SMenu = toolbar.addMenu('Santosh Tools', icon='V_Victor.png')
+# Santosh Menubar
+SantoshMenu = menubar.addMenu("$antosh")
+SantoshMenu.addCommand("Dummy", 'nuke.createNode("Write")', 'alt+y')
+
+# Santosh inside Toolbar; this is for gizmos
+toolbar = nuke.menu('Nodes')
+S_Nodes_Menu = toolbar.addMenu('$antoshTools', icon='S_Santosh.png')
+S_Nodes_Menu.addCommand('S_Dummy', 'nuke.createNode("Write")', 'ctrl+b', icon='S_Dummy.png')
--- python/dummy.py
+++ python/dummy.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
 
-def dummyFunc():
-	return "Dummy is being called"
+def dummyFunc(x):
+	print("Dummy function was called from..", x)
+	return "Dummy function was called from..", x
