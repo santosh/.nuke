c2deb7c5804cb5ba8b6e18e8098e8072c917d81d
Santosh Kumar <santosh@users.noreply.github.com>
2017-08-23 14:51:32 +0530

    Update connectNodes.py

--- playground/connectNodes.py
+++ playground/connectNodes.py
@@ -5,7 +5,7 @@ flower = nuke.toNode('Read1')
 cc = nuke.createNode('ColorCorrect')
 
 # connect cc node to flower
-# 0 is the first parameter, which defines it is the first
+# 0 is the first parameter if you want to connect to first input
 cc.setInput(0, flower)
 cc.knob('shadows.gain').setValue(0.02)
 
