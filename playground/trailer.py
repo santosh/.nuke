d23de8cf78328fa08cb8004bc6d457884943e39a
Santosh Kumar <me@sntsh.com>
2017-09-18 03:28:15 +0530

    wrapped up inside group/gizmo
    with no controls

--- playground/trailer.py
+++ playground/trailer.py
@@ -1,9 +1,16 @@
+# find the tutorial at nukestation.com/python-scripting-in-nuke-part-1-of-2-creating-nodes/
+
 # using naming convention, like i b n for int, bool, node
 
-iRepeat = 10 #probably gonna be replaced by knob input
+iRepeat = 10 # probably gonna be replaced by knob input
 bFirstLoop = True
 
+nGroup = nuke.nodes.Group(name="Trails")
+nGroup.begin()
+
+nInput = nuke.nodes.Input()
 nDot = nuke.nodes.Dot()
+nDot.setInput(0, nInput)
 
 for i in range(iRepeat):
     # translating by 20 in x and y
@@ -14,7 +21,7 @@ for i in range(iRepeat):
     nMerge.setInput(1, nTrans)
 
     if bFirstLoop: # if this is the first loop
-        bFirstLoop = False # it will no longer be first loop for rest of the script
+        bFirstLoop = False # disable the logic, so below commands execute only once
         nTrans.setInput(0, nDot) # connect tranlate node to dot
         nMerge.setInput(0, nDot) # then merge to dot
     else: # do the same thing, but with previous merges
@@ -23,3 +30,8 @@ for i in range(iRepeat):
     
     # nMerge becomes nPrevMerge for next Merge2
     nPrevMerge = nMerge
+
+nOutput = nuke.nodes.Output()
+nOutput.setInput(0, nMerge)
+
+nGroup.end()
