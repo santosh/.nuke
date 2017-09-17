5961e44a561dc3f02e997db1418421a717dc88a4
Santosh Kumar <me@sntsh.com>
2017-09-18 01:47:12 +0530

    prototypes future methods

--- python/S_Utils.py
+++ python/S_Utils.py
@@ -13,3 +13,20 @@ def Scene_3D():
 def disable(x):
 	'''Disables X nodes, defaults to RotoPaint'''
 	
+	# if nuke.ask('Disable all RotoPaint Nodes GUI?'):
+	# 	for a in nuke.allNodes('RotoPaint'):
+	# 		a['disable'].setExpression('$gui')
+
+def delete_disable():
+	'''Deletes disable nodes'''
+
+def propose():
+	nuke.message("I love you!")
+	nuke.ask("Do you love me too?")
+
+# Here for templating purpose only
+
+# Delete all nodes except x
+# for i in nuke.allNodes():
+# 	if i.Class() != 'Read':
+# 		nuke.delete(i)
