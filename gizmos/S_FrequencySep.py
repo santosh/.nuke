55083aa0584605eef92920cf79077ce36ec293aa
Santosh Kumar <me@sntsh.com>
2017-12-24 20:07:31 +0530

    adds tooltip to S_FrequencySep knobs
     * Also updates README to add introduction paragraph.

--- gizmos/S_FrequencySep.py
+++ gizmos/S_FrequencySep.py
@@ -10,11 +10,14 @@ nGroup.begin() # gizmo starts
 kDetails = nuke.Double_Knob("details", "Details:")
 kDetails.setRange(0,30.)
 kDetails.setValue(0)
+kDetails.setTooltip("Details to retain. Higher value goes back to original image. \
+	Lowest point is inverse value of smoothness.")
 nGroup.addKnob(kDetails)
 
 kSmoothness = nuke.Double_Knob("smoothness", "Smoothness:")
 kSmoothness.setRange(0,100.)
 kSmoothness.setValue(0)
+kSmoothness.setTooltip("Smoothness to achieve. Lower value goes back to original.")
 nGroup.addKnob(kSmoothness)
 
 ## GUI COMPONENTS END
@@ -26,7 +29,6 @@ nMergeFrom = nuke.nodes.Merge(operation="from")
 nMergeFrom.setInput(0, nInput)
 
 nBlurDetails = nuke.nodes.Blur(label="Details",
-							tooltip="Details to preserve.",
 							size="parent.details")
 nBlurDetails.setInput(0, nInput)
 nMergeFrom.setInput(1, nBlurDetails)
@@ -34,7 +36,6 @@ nMergeFrom.setInput(1, nBlurDetails)
 nInputMask = nuke.nodes.Input(name="mask")
 
 nBlurSmoothness = nuke.nodes.Blur(label="Smoothness",
-								tooltip="Amount of smoothness to achieve.",
 								size="parent.smoothness")
 nBlurSmoothness.setInput(0, nBlurDetails)
 nBlurSmoothness.setInput(1, nInputMask)
